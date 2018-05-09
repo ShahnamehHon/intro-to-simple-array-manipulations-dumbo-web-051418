8232088c70bd9765328df21de81da744667edbdc
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-09 16:01:57 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,7 +18,14 @@ def using_shift(arr)
   arr.shift
 end
 
+def shift_with_arg(arr)
+  arr.shift(2)
+end
+  
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
 def using_push(arr, str)
   arr.push(str)
 end
-  
