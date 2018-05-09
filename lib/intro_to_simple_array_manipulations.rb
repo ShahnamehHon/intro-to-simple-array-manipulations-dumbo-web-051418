3a4122279561ef26fd208868b0bf06d1efd1e259
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-09 15:54:18 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,17 +1,1 @@
-def using_push(arr, str)
-countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-next_country = "Niger"
-countries_in_western_africa.push(next_country)
-end
-
-def using_unshift(arr, str)
-neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-new_nightborhoods = "Brooklyn Heights"
-neighborhoods_in_northwest_brooklyn.unshift(new_nightborhoods)
-end
-
-def using_pop(arr, str)
-  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  great_hits_of_the_nineties.pop(str)
-end
-
+def 
