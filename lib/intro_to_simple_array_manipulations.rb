f84f25da4da0bd36b290a1e1cf540d15a19338c6
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-09 15:38:59 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,12 @@
+def using_push(arr, str)
+countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+next_country = "Niger"
+countries_in_western_africa.push(next_country)
+end
+
+def using_unshift(arr, str)
+neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+new_nightborhoods = "Brooklyn Heights"
+neighborhoods_in_northwest_brooklyn.unshift(new_nightborhoods)
+end
+
