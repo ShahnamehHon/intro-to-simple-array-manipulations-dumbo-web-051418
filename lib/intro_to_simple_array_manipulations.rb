d009fc61d9e508d96211a6426f75d78289c6f3e8
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-09 15:49:04 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,6 +12,6 @@ end
 
 def using_pop(arr, str)
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  great_hits_of_the_nineties.pop
+  return great_hits_of_the_nineties.pop
 end
 
